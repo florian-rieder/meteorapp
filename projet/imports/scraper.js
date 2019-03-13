ad84421a03f80a69334f58f7ab759453db59924e
Sergenti <florian.rieder@unil.ch>
2019-03-13 10:25:50 +0100

    puppeteer screenshot

---
+++ projet/imports/scraper.js
@@ -0,0 +1,9 @@
+import puppeteer from 'puppeteer';
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://google.com');
+	await page.screenshot({path: '../../../../../example.png'});
+  await browser.close();
+})();
--- projet/server/main.js
+++ projet/server/main.js
@@ -1,5 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 
+import '../imports/scraper.js';
+
 Meteor.startup(() => {
   // code to run on server at startup
 });

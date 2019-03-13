a4e3326ed60b97df9788069568b882c9e40d5d40
Sergenti <florian.rieder@unil.ch>
2019-03-13 10:44:56 +0100

    scraper ingrédients
    Scrape les ingrédients du dafalgan sur compendium.ch

--- projet/imports/scraper.js
+++ projet/imports/scraper.js
@@ -3,7 +3,10 @@ import puppeteer from 'puppeteer';
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://google.com');
-	await page.screenshot({path: '../../../../../example.png'});
+	await page.goto('https://compendium.ch/prod/co-dafalgan-cpr-eff-500-30mg/fr');
+	const compounds = await page.evaluate(
+		() => document.querySelector('#compEverything > table').innerHTML
+	);
+	console.log('compounds', compounds);
   await browser.close();
 })();

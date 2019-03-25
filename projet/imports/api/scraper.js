5475b474b99e0dfad050431641200a9ff055f673
Sergenti <florian.rieder@unil.ch>
2019-03-25 14:54:24 +0100

    added comments

--- projet/imports/api/scraper.js
+++ projet/imports/api/scraper.js
@@ -14,6 +14,7 @@ Meteor.methods({
 	}
 })
 
+// fetches the name, composition and notice from a drug's page on compendium URL
 async function scrapeDrug(compendiumURL) {
 	const titleSelector = '#ctl00_MainContent_ucProductDetail1_dvProduct_lblProductDescr';
 	// launch puppeteer browser
@@ -62,11 +63,12 @@ async function scrapeDrug(compendiumURL) {
 
 	return drugData;
 }
-
+// fetches the results of the search of any string sent to compendium.ch search engine
 async function searchByString(searchString) {
+	// convert the searchString into a format suitable to be injected in an URL
 	const newString = searchString.split(' ').join('!20');
 	const searchURL = `https://compendium.ch/search/${newString}/fr`;
-
+	// launch puppeteer browser
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
 	await page.goto(searchURL);
@@ -75,14 +77,16 @@ async function searchByString(searchString) {
 	// await page load
 	await page.waitForSelector(trSelector);
 
-	// get the title (e.g. dafalgan), subtitle (e.g. paracetamol), and the path to access
+	// get the title (e.g. dafalgan), subtitle (e.g. paracetamol), and the URL path to access
 	// the details of the drug (that we can transform to get the compendiumURL parameter 
 	// of scrapeDrug(compendiumURL))
 	const searchResults = await page.evaluate((selector) => {
 		return Array.from(document.querySelectorAll(selector))
 			.map((resultEntry) => {
+				// complicated stuff to access the info we need in the page
 				let tdThatContainsTheInfoWeNeed = Array.from(Array.from(resultEntry.childNodes)[2].childNodes);
 				let informationsContainer = Array.from(tdThatContainsTheInfoWeNeed[1].childNodes)
+				// create an easy to handle object to return
 				const resultObject = {
 					title: informationsContainer[0].textContent,
 					subtitle: informationsContainer[4].textContent,

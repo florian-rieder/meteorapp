8276b6708809f20d3bfdd724679c3fab95ff7bb4
Pedro.Tome <tome.pedro.sierre@gmail.com>
2019-04-02 15:03:54 +0200

    Added window navigation interface

--- projet/imports/ui/scripts/footerBar.js
+++ projet/imports/ui/scripts/footerBar.js
@@ -7,6 +7,36 @@ Template.footerBar.helpers({
 		{name: 'Pharmacie'},
 		{name: 'Recherche'},
 		{name: 'Scan'},
-		{name: 'Aide'},
+		{name: 'Notice'},
 	]
 })
+
+Template.navButton.events({
+	'click .navButton_button' () {
+		switch(this.name) {
+			case 'Profil':
+				changeWindow('myProfile');
+				break;
+			case 'Pharmacie':
+				changeWindow('myPharmacy');
+				break;
+			case 'Recherche':
+				changeWindow('searchPage');
+				break;
+			case 'Scan':
+				changeWindow('scannerWindow');
+				break;		
+			case 'Notice':
+				changeWindow('drugDataWindow');
+		}
+	} 
+})
+
+function changeWindow(buttonID){
+	let windowsArray = Array.from(document.querySelectorAll('.navWindow'));
+	console.log('Got here', windowsArray);
+	windowsArray.forEach((e)=>e.classList.add('hidden'));
+	document.getElementById(buttonID).classList.remove('hidden');
+}
+
+

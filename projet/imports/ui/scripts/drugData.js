740305be85a9c0457ab16b728a7e056a26e33c32
Sergenti <florian.rieder@unil.ch>
2019-04-03 15:31:35 +0200

    added add button to drug inspection from search

--- projet/imports/ui/scripts/drugData.js
+++ projet/imports/ui/scripts/drugData.js
@@ -1,10 +1,14 @@
 import { Template } from 'meteor/templating';
 import '../templates/drugData.html';
 import { changeWindow, inspectDrugData, lastActivePage } from '../../api/utilities.js';
+import { Drugs } from '../../api/collections';
 
 Template.drugData.helpers({
 	drugData() {
 		return inspectDrugData.get();
+	},
+	originIsSearchInspect(){
+		return lastActivePage.get() == 'windowRecherche';
 	},
 	//notice formatting
 	isTitleFromIndex(index){
@@ -18,5 +22,8 @@ Template.drugData.helpers({
 Template.drugData.events({
 	'click #backButton' (){
 		changeWindow(lastActivePage.get());
+	},
+	'click #addDrugToPharmacyButton'(){
+		Meteor.call('drugs.insert', inspectDrugData.get(), () => alert('Médicament ajouté à votre pharmacie'));
 	}
 })

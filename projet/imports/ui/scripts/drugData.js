6ce239f13b0682ee411803a5df37b4ba8e60c863
Sergenti <florian.rieder@unil.ch>
2019-04-03 10:08:47 +0200

    Update drugData.js

--- projet/imports/ui/scripts/drugData.js
+++ projet/imports/ui/scripts/drugData.js
@@ -7,10 +7,11 @@ Template.drugData.helpers({
 	drugData() {
 		return TempDrugInspected.findOne({});
 	},
+	//notice formatting
 	isTitleFromIndex(index){
 		return index == 0;
 	},
 	isOfTwoLastFromIndex(index){
-		return index > TempDrugInspected.findOne({}).notice.length-3; 
+		return index > TempDrugInspected.findOne({}).notice.length-3;
 	}
 });

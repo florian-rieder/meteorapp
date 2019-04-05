5fcee81b36abfa0d89d1c12b705464afc3741bef
Sergenti <florian.rieder@unil.ch>
2019-04-05 15:19:48 +0200

    tweaks to search page
    - Pressing enter key when writing in searchbar will now trigger the search
    - searching for a term will instantly erase all previous searchresults to empty the results page

--- projet/imports/ui/scripts/searchBar.js
+++ projet/imports/ui/scripts/searchBar.js
@@ -7,22 +7,36 @@ Template.searchBar.events({
 	'click .searchBar_searchButton'(e) {
 		e.preventDefault();
 		//show loading wheel
-		LoadingWheel.show();
-		/* call searchDrug with the value of the text input, log the result */
-		Meteor.call('searchDrug', document.querySelector('.searchBar_searchSquare').value, (error, result) => {
-			// hide loading wheel
-			LoadingWheel.hide();
-			if (result) {
-				console.log(result.length + ' results');
-				/* add all the results of the search to SearchResults */
-				searchResults.set(result);
-			}
-			if (error) {
-				swal({
-					title: "Une erreur s'est produite",
-					icon: 'error',
-				});
-			}
-		});
+		search();
+	},
+	//trigger the search when the enter key is pressed
+	'keyup .searchBar_searchSquare'(e) {
+		if (e.keyCode == 13) {// ENTER
+			search();
+		}
 	}
-})
+});
+
+
+function search() {
+	// empty searchResults (to hide results from page)
+	searchResults.set(undefined);
+	// show loading wheel
+	LoadingWheel.show();
+	/* call searchDrug with the value of the text input, log the result */
+	Meteor.call('searchDrug', document.querySelector('.searchBar_searchSquare').value, (error, result) => {
+		// hide loading wheel
+		LoadingWheel.hide();
+		if (result) {
+			console.log(result.length + ' results');
+			/* add all the results of the search to SearchResults */
+			searchResults.set(result);
+		}
+		if (error) {
+			swal({
+				title: "Une erreur s'est produite",
+				icon: 'error',
+			});
+		}
+	});
+}

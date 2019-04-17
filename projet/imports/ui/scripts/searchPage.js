8527224ce9cd9fddd77ca1cd1be29dac78a02456
Pedro.Tome <tome.pedro.sierre@gmail.com>
2019-04-17 07:38:42 +0200

    Added first routing features (to be configured), jointed searchBar and searchResults in a searchPage template

--- projet/imports/ui/scripts/searchPage.js
+++ projet/imports/ui/scripts/searchPage.js
@@ -1,11 +1,11 @@
-import '../templates/searchResults.html';
+import '../templates/searchPage.html';
 import './drugData.js';
-
+import '../../api/collections.js';
 import { Template } from 'meteor/templating';
 import { changeWindow, inspectDrugData, searchResults, LoadingWheel, fireDrugAddDialog } from '../../api/utilities';
 import Swal from 'sweetalert2';
 
-Template.searchResults.helpers({
+Template.searchPage.helpers({
 	results() {
 		// get all items in collection SearchResults
 		return searchResults.get();
@@ -77,3 +77,49 @@ Template.result.events({
 		});
 	}
 })
+
+Template.searchBar.events({
+	'click .searchBar_searchButton'(e) {
+		e.preventDefault();
+		//show loading wheel
+		search();
+	},
+	//trigger the search when the enter key is pressed
+	'keyup .searchBar_searchSquare'(e) {
+		if (e.keyCode == 13) {// ENTER
+			search();
+		}
+	}
+});
+
+function search() {
+	// empty searchResults (to hide results from page)
+	searchResults.set(undefined);
+	// show loading wheel
+	LoadingWheel.show();
+	/* call searchDrug with the value of the text input, log the result */
+	Meteor.call('searchDrug', document.querySelector('.searchBar_searchSquare').value, (error, result) => {
+		// hide loading wheel
+		LoadingWheel.hide();
+		if (result) {
+			console.log(result.length + ' results');
+			if(result[0] == undefined){
+				Swal.fire({
+					title: "Nous n'avons trouvé aucun résultat",
+				});
+				// prevents the "Nous avons trouvé 0 resultats." from showing up, since
+				// we have an alert for that
+				result = undefined;
+			}
+			/* add all the results of the search to SearchResults */
+			searchResults.set(result);
+		}
+		if (error) {
+			Swal.fire({
+				title: "Une erreur s'est produite",
+				text: error.message,
+				type: 'error',
+			});
+		}
+	});
+}

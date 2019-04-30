53b775fc4b99cf44ab7e40ee65eafd10ed08c600
Pedro.Tome <tome.pedro.sierre@gmail.com>
2019-04-30 14:57:52 +0200

    Solution to profile database problem !
    Thank you Florian for the useful help !
    
    Co-Authored-By: Florian <florian.rieder@unil.ch>

--- projet/imports/ui/scripts/applicationLayout.js
+++ projet/imports/ui/scripts/applicationLayout.js
@@ -1,7 +1,4 @@
-import { Meteor } from 'meteor/meteor';
-import Swal from 'sweetalert2';
 import { lastActivePage, search, inspectDrugData } from '../../api/utilities.js';
-
 import '../templates/footerBar.html';
 import '../templates/drugsList.html';
 import '../templates/helpPage.html';
@@ -79,38 +76,6 @@ Router.route('/details/:_id', function () {
 
 
 Router.route('/profile', function () {
-	// Show dialog on page enter if user has no profile
-	Meteor.call('profile.count', (error, count) => {
-		if (!error && count === 0) {
-			// if the user has not made a profile yet, show a dialog asking if the user wants to create one
-			Swal.fire({
-				type: 'warning',
-				title: "Aucun profil détecté ! Souhaitez-vous en créer un ?",
-				// cancel button
-				showCancelButton: true,
-				cancelButtonText: 'Non',
-				// confirm button
-				confirmButtonText: 'Oui !',
-				confirmButtonColor: 'green',
-
-			}).then(result => {
-				// If the confirm button was pressed
-				if (result.value) {
-					// remove hidden tag
-					document.getElementById('profile_container').classList.remove('hidden');
-				}
-			});
-		} else {
-			let profileObj = Profile.findOne();
-			/* for(let i=0; i<8; ++i) {
-
-			} */
-			document.getElementById('profile_container').classList.remove('hidden');
-		}
-		if (error) {
-			console.error(error);
-		}
-	});
-	this.render('profile');
+	this.render('profile',{data: Profile.findOne()});
 	this.render('footerBar', { to: 'footer' });
 });
--- projet/imports/ui/scripts/profile.js
+++ projet/imports/ui/scripts/profile.js
@@ -4,6 +4,7 @@ import '../templates/profile.html';
 import { ReactiveVar } from 'meteor/reactive-var';
 import { Meteor } from 'meteor/meteor';
 import { Profile } from '../../api/collections';
+import Swal from 'sweetalert2';
 //uncomment to clear Db 
 //Meteor.call('profile.remove');
 
@@ -17,26 +18,60 @@ Template.uploadForm.helpers({
   }
 });
 
+Template.profile.onCreated(function () {
+  // Show dialog on page enter if user has no profile
+  Meteor.call('profile.count', (error, count) => {
+    if (!error && count === 0) {
+      // if the user has not made a profile yet, show a dialog asking if the user wants to create one
+      Swal.fire({
+        type: 'warning',
+        title: "Aucun profil détecté ! Souhaitez-vous en créer un ?",
+        // cancel button
+        showCancelButton: true,
+        cancelButtonText: 'Non',
+        // confirm button
+        confirmButtonText: 'Oui !',
+        confirmButtonColor: 'green',
+
+      }).then(result => {
+        // If the confirm button was pressed
+        if (result.value) {
+          // remove hidden tag
+        }
+      });
+    } else {
+
+    }
+  });
+});
 Template.profile.helpers({
-  fields() {
-    return [
-      { fieldName: 'Sexe' },
-      { fieldName: 'Prénom' },
-      { fieldName: 'Nom' },
-      { fieldName: 'Age' },
-      { fieldName: 'Taille' },
-      { fieldName: 'Poids' },
-      { fieldName: 'Numéro AVS' },
-    ]
+  fields: [
+      { fieldName: 'Sexe',
+        fieldId: 'sex' },
+      { fieldName: 'Prénom', 
+        fieldId: 'fn'},
+      { fieldName: 'Nom',
+        fieldId: 'ln' },
+      { fieldName: 'Age',
+        fieldId: 'age' },
+      { fieldName: 'Taille',
+        fieldId: 'hgt' },
+      { fieldName: 'Poids',
+        fieldId: 'wgt' },
+      { fieldName: 'Numéro AVS',
+        fieldId: 'avs' },
+    ],
+  hasProfileData() {
+    return Template.instance().data != undefined;
+  },
+  getProfileField(index){
+    return Template.instance().data[index];
   }
 });
 
 Template.profile.events({
   'click #confirmButton'() {
     let profileArray = Array.from(document.querySelectorAll('.field_textInput')).map(v => v.value);
-    console.log(profileArray);
     Meteor.call('profile.insert', profileArray);
-    console.log('Added');
-    console.log(Profile.findOne());
   }
 });

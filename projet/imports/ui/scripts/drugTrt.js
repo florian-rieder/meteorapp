7ced09f4f7e340ddedbfe2077d5e004994ec8221
Pedro.Tome <tome.pedro.sierre@gmail.com>
2019-05-26 21:55:08 +0200

    Minor formatting

--- projet/imports/ui/scripts/drugTrt.js
+++ projet/imports/ui/scripts/drugTrt.js
@@ -8,68 +8,68 @@ let grid = new ReactiveVar(undefined);
 let currentDrug = undefined;
 
 Template.drugTrt.helpers({
-    weekDays: [
-        {day: 'L'},
-        {day: 'M'},
-        {day: 'M'},
-        {day: 'J'},
-        {day: 'V'},
-        {day: 'S'},
-        {day: 'D'},
+	weekDays: [
+		{ day: 'L' },
+		{ day: 'M' },
+		{ day: 'M' },
+		{ day: 'J' },
+		{ day: 'V' },
+		{ day: 'S' },
+		{ day: 'D' },
 
-    ],
-    timeStamp: [
-        {time: '00:00'},
-        {time: '02:00'},
-        {time: '04:00'},
-        {time: '08:00'},
-        {time: '10:00'},
-        {time: '12:00'},
-        {time: '14:00'},
-        {time: '16:00'},
-        {time: '18:00'},
-        {time: '20:00'},
-        {time: '22:00'},
-		],
-		grid(){
-			// setup variables in helper, because for some reason it doesn't work in onCreated
-			currentDrug = Template.instance().data;
-			grid.set(Template.instance().data.treatmentGrid);
-			// return grid
-			return grid.get();
-		},
-		getTime(index){
-			let timeStamps = [
-        {time: '00:00'},
-        {time: '02:00'},
-				{time: '04:00'},
-				{time: '06:00'},
-        {time: '08:00'},
-        {time: '10:00'},
-        {time: '12:00'},
-        {time: '14:00'},
-        {time: '16:00'},
-        {time: '18:00'},
-        {time: '20:00'},
-        {time: '22:00'},
+	],
+	timeStamp: [
+		{ time: '00:00' },
+		{ time: '02:00' },
+		{ time: '04:00' },
+		{ time: '08:00' },
+		{ time: '10:00' },
+		{ time: '12:00' },
+		{ time: '14:00' },
+		{ time: '16:00' },
+		{ time: '18:00' },
+		{ time: '20:00' },
+		{ time: '22:00' },
+	],
+	grid() {
+		// setup variables in helper, because for some reason it doesn't work in onCreated
+		currentDrug = Template.instance().data;
+		grid.set(Template.instance().data.treatmentGrid);
+		// return grid
+		return grid.get();
+	},
+	getTime(index) {
+		let timeStamps = [
+			{ time: '00:00' },
+			{ time: '02:00' },
+			{ time: '04:00' },
+			{ time: '06:00' },
+			{ time: '08:00' },
+			{ time: '10:00' },
+			{ time: '12:00' },
+			{ time: '14:00' },
+			{ time: '16:00' },
+			{ time: '18:00' },
+			{ time: '20:00' },
+			{ time: '22:00' },
 		];
 		return timeStamps[index].time;
-		}
+	}
 });
 
 Template.hourCell.helpers({
-	isChecked(){
+	isChecked() {
 		return Template.instance().data.checked;
 	}
 })
 
 Template.hourCell.events({
-	'click .cell'(e){
+	'click .cell'(e) {
 		e.preventDefault();
 		let data = Template.instance().data;
 
 		// toggle checked boolean
-		if(data.checked){
+		if (data.checked) {
 			data.checked = false;
 		} else {
 			data.checked = true;
@@ -79,7 +79,7 @@ Template.hourCell.events({
 		let totalGrid = grid.get();
 		totalGrid[data.pos.time][data.pos.day] = data;
 		grid.set(totalGrid);
-		
+
 		// update drug in db
 		currentDrug.treatmentGrid = grid.get();
 		Meteor.call('drugs.update', currentDrug._id, currentDrug);

b8e1af9d5933eba58f16b25b8880fc7bf3cb3536
Sergenti <florian.rieder@unil.ch>
2019-05-26 15:24:56 +0200

    minor modifications
    - removed style='height: 100%' from quagScan, as it was preventing the video feed from showing on my phone
    - Put permissions check in startScanner()
    - css corrections on drugsList
    - corrected image in drugData squeezing on small screens

--- projet/imports/ui/scripts/quagga.js
+++ projet/imports/ui/scripts/quagga.js
@@ -7,196 +7,170 @@ import '../templates/applicationLayout.html';
 // Flag for scanner status
 let scannerIsRunning = false;
 
-// start/stop scanner
-Template.quagScan.events({
-	'click #scan_btn'() {
-		if (Meteor.isCordova) {
-			var permissions = cordova.plugins.permissions;
-			// check if the app has permission to use the camera
-			// Fix for NotReadableError: video source, don't forget to add Camera Permissions in Android Manifest !!!
-			permissions.checkPermission(permissions.CAMERA, function (status) {
-				if (status.hasPermission) {
-					console.log("camera permission allowed");
-					toggleScan();
-				}
-				else {
-					console.warn("camera permission not allowed");
-					// if permission is not allowed, request permission to use the camera
-					permissions.requestPermission(permissions.CAMERA, 
-						() => {
-							// permission granted
-							console.log('permission granted.');
-							toggleScan();
-						}, 
-						() => {
-							// permission refused
-							console.log('permission refused.');
-						});
-				}
-			});
-		} else {
-			toggleScan();
-		}
-
-		function toggleScan() {
-			if (scannerIsRunning) {
-				stopScanner();
-			} else {
-				startScanner();
-			}
-			console.log('quagga running: ' + scannerIsRunning);
-		}
-	}
-});
-
 export function startScanner() {
-	const bodyWidth = $('main').width();
-	const bodyHeight = $('main').height();
-	console.log('width: ' + bodyWidth + ' height: ' + bodyHeight);
-	// initialization of scanner config
-	Quagga.init({
-		inputStream: {
-			name: "Live",
-			type: "LiveStream",
-			target: document.querySelector('#scannerContainer'),
-			constraints: {
-				width: bodyWidth,
-				height: bodyHeight,
-				facingMode: 'environment',
+	// permissions check if mobile
+	if(Meteor.isCordova){
+		var permissions = cordova.plugins.permissions;
+		// check if the app has permission to use the camera
+		// Fix for NotReadableError: video source, don't forget to add Camera Permissions in Android Manifest !!!
+		permissions.checkPermission(permissions.CAMERA, function (status) {
+			if (status.hasPermission) {
+				console.log("camera permission allowed");
+				start();
+			}
+			else {
+				console.warn("camera permission not allowed");
+				// if permission is not allowed, request permission to use the camera
+				permissions.requestPermission(permissions.CAMERA,
+					() => {
+						// permission granted
+						console.log('permission granted.');
+						start();
+					},
+					() => {
+						// permission refused
+						console.log('permission refused.');
+					});
+			}
+		});
+	} else {
+		start();
+	}
+
+	function start() {
+		const mainWidth = $('main').width();
+		const mainHeight = $('main').height();
+		console.log('width: ' + mainWidth + ' height: ' + mainHeight);
+		// initialization of scanner config
+		Quagga.init({
+			inputStream: {
+				name: "Live",
+				type: "LiveStream",
+				target: document.querySelector('#scannerContainer'),
+				constraints: {
+					width: mainWidth,
+					height: mainHeight,
+					facingMode: 'environment',
+				},
+			},
+			frequency: 20,
+			singleChannel: true,
+			locator: {
+				halfSample: false,
+				patchSize: "medium", // x-small, small, medium, large, x-large
+				debug: {
+					showCanvas: false,
+					showPatches: false,
+					showFoundPatches: false,
+					showSkeleton: true,
+					showLabels: false,
+					showPatchLabels: false,
+					showRemainingPatchLabels: false,
+					boxFromPatches: {
+						showTransformed: false,
+						showTransformedBox: false,
+						showBB: false
+					}
+				}
+			},
+			numOfWorkers: window.navigator.hardwareConcurrency,
+			/* area: { // defines rectangle of the detection/localization area
+				top: "10%",    // top offset
+				right: "15%",  // right offset
+				left: "15%",   // left offset
+				bottom: "10%"  // bottom offset
+				}, */
+			//Sets types of barcodes supported
+			decoder: {
+				readers: [
+					// order matters, and do not add unnecessary readers
+					"ean_reader",
+				],
 			},
 		},
-		frequency: 20,
-		singleChannel: true,
-		locator: {
-			halfSample: false,
-			patchSize: "medium", // x-small, small, medium, large, x-large
-			debug: {
-				showCanvas: false,
-				showPatches: false,
-				showFoundPatches: false,
-				showSkeleton: true,
-				showLabels: false,
-				showPatchLabels: false,
-				showRemainingPatchLabels: false,
-				boxFromPatches: {
-					showTransformed: false,
-					showTransformedBox: false,
-					showBB: false
+			function (err) {
+				if (err) {
+					console.error(err.name + ': ' + err.message);
+					return;
 				}
-			}
-		},
-		numOfWorkers: window.navigator.hardwareConcurrency,
-		/* area: { // defines rectangle of the detection/localization area
-			top: "10%",    // top offset
-			right: "15%",  // right offset
-			left: "15%",   // left offset
-			bottom: "10%"  // bottom offset
-		  }, */
-		//Sets types of barcodes supported
-		decoder: {
-			readers: [
-				// order matters, and do not add unnecessary readers
-				"ean_reader",
-			],
-		},
-	},
-		function (err) {
-			if (err) {
-				console.error(err.name + ': ' + err.message);
-				return;
-			}
 
-			console.log("Initialization finished. Ready to start");
-			Quagga.start();
+				console.log("Initialization finished. Ready to start");
+				Quagga.start();
 
-			// Set flag to is running
-			scannerIsRunning = true;
+				// Set flag to is running
+				scannerIsRunning = true;
+			});
+
+		Quagga.onProcessed(function (result) {
+			//sets context and canvas for quagga overlay
+			var drawingCtx = Quagga.canvas.ctx.overlay,
+				drawingCanvas = Quagga.canvas.dom.overlay;
+
+			//Draws box around codebar for visual feedback of scanner, different visual feedback depending on result type
+			if (result) {
+				//General detection of barcode
+				if (result.boxes) {
+					drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+					result.boxes.filter(function (box) {
+						return box !== result.box;
+					}).forEach(function (box) {
+						Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, { color: "black", lineWidth: 2 });
+					});
+				}
+				//precise detection of barcode
+				if (result.box) {
+					Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, { color: "white", lineWidth: 2 });
+				}
+				//Red line indicating full detection process
+				/* if (result.codeResult && result.codeResult.code) {
+					Quagga.ImageDebug.drawPath(result.line, { x: 'x', y: 'y' }, drawingCtx, { color: 'rgb(71, 91, 206)', lineWidth: 3 });
+				} */
+			}
 		});
 
-	Quagga.onProcessed(function (result) {
-		//sets context and canvas for quagga overlay
-		var drawingCtx = Quagga.canvas.ctx.overlay,
-			drawingCanvas = Quagga.canvas.dom.overlay;
+		let results = [];
+		let detectedBarCodes = 0;
+		const samples = 5;
 
-		//Draws box around codebar for visual feedback of scanner, different visual feedback depending on result type
-		if (result) {
-			//General detection of barcode
-			if (result.boxes) {
-				drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
-				result.boxes.filter(function (box) {
-					return box !== result.box;
-				}).forEach(function (box) {
-					Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, { color: "black", lineWidth: 2 });
-				});
-			}
-			//precise detection of barcode
-			if (result.box) {
-				Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, { color: "white", lineWidth: 2 });
-			}
-			//Red line indicating full detection process
-			/* if (result.codeResult && result.codeResult.code) {
-				Quagga.ImageDebug.drawPath(result.line, { x: 'x', y: 'y' }, drawingCtx, { color: 'rgb(71, 91, 206)', lineWidth: 3 });
-			} */
-		}
-	});
+		Quagga.onDetected(function (result) {
+			results.push(result.codeResult.code);
 
-	let results = [];
-	let detectedBarCodes = 0;
-	const samples = 5;
-
-	Quagga.onDetected(function (result) {
-		results.push(result.codeResult.code);
-		
-		if (detectedBarCodes === samples) {
-			// if we scanned $samples bar codes, stop the scanner
-			stopScanner();
-			// get the most occuring result in the array (get its mode)
-			const mostOccurringBarcode = mode(results);
-			// use the most occurring barcode for stuff
-			console.log('most occurring barcode: ' + mostOccurringBarcode);
-			searchCode(mostOccurringBarcode);
-			// reset variables
-			results = [];
-			detectedBarCodes = 0;
-		} else {
-			detectedBarCodes++;
-		}
-
-		function mode(array) {
-			let processed = {};
-			// for each result, we count the number of occurrences
-			array.forEach(result => {
-				// we use an object whose properties are the value of the result, that way we
-				// can create a new property called `_${result}` (because we can't use numbers as variable names)
-				// and we then count the number of occurrences of all results.
-				if (processed[`_${result}`] === undefined) { // strict equality
-					processed[`_${result}`] = 1;
-				} else {
-					processed[`_${result}`]++;
-				}
-			});
-
-			/* //https://github.com/serratus/quaggaJS/issues/237
-			var countDecodedCodes = 0, err = 0;
-			$.each(result.codeResult.decodedCodes, function (id, error) {
-				if (error.error != undefined) {
-					countDecodedCodes++;
-					err += parseFloat(error.error);
-				}
-			});
-			if (err / countDecodedCodes < 0.1) {
-				// correct code detected
+			if (detectedBarCodes === samples) {
+				// if we scanned $samples bar codes, stop the scanner
+				stopScanner();
+				// get the most occuring result in the array (get its mode)
+				const mostOccurringBarcode = mode(results);
+				// use the most occurring barcode for stuff
+				console.log('most occurring barcode: ' + mostOccurringBarcode);
+				searchCode(mostOccurringBarcode);
+				// reset variables
+				results = [];
+				detectedBarCodes = 0;
 			} else {
-				// probably wrong code
+				detectedBarCodes++;
 			}
-			//end  */
 
-			// it was so beautiful as a one-liner that I simply had to factor it like that 
-			// (sorry for readability... tl;dr: returns the barcode with the highest count)
-			return Object.getOwnPropertyNames(processed).reduce((mode, code) => processed[mode] < processed[code] ? code : mode)
-				.slice(1); // remove underscore
-		}
-	});
+			function mode(array) {
+				let processed = {};
+				// for each result, we count the number of occurrences
+				array.forEach(result => {
+					// we use an object whose properties are the value of the result, that way we
+					// can create a new property called `_${result}` (because we can't use numbers as variable names)
+					// and we then count the number of occurrences of all results.
+					if (processed[`_${result}`] === undefined) { // strict equality
+						processed[`_${result}`] = 1;
+					} else {
+						processed[`_${result}`]++;
+					}
+				});
+
+				// it was so beautiful as a one-liner that I simply had to factor it like that 
+				// (sorry for readability... tl;dr: returns the barcode with the highest count)
+				return Object.getOwnPropertyNames(processed).reduce((mode, code) => processed[mode] < processed[code] ? code : mode)
+					.slice(1); // remove underscore
+			}
+		});
+	}
 }
 
 export function stopScanner() {
@@ -210,11 +184,11 @@ function searchCode(code) {
 	LoadingWheel.show();
 	// we found out that searching for the code in compendium search engine returned the drug
 	Meteor.call('searchDrug', code, (err, res) => {
-		LoadingWheel.hide();
 		if (res) {
 			if (res[0]) {
 				// so we take the first element of search results and scrape it
 				Meteor.call('scrapeDrug', `http://www.compendium.ch${res[0].path}`, (error, result) => {
+					LoadingWheel.hide();
 					if (result) {
 						inspectDrugData.set(result);
 						Router.go('/details');
